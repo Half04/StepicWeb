32f51e24d30d719b3c063ceb55cf25f0ee10d075
vi-Ubuntu <viktors.zakharov@gnail.com>
2019-11-14 16:45:37 +0300

    Launch gunicorn without config path

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c test hello:app
+sudo gunicorn hello:app
 
