2a33ec0789e84aaacb4bfd821cc3363ab1f70bbb
vi-Ubuntu <viktors.zakharov@gnail.com>
2019-12-24 00:26:06 +0300

    (+) 2.1 Step 11

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3 
+
 def app(environ, start_response):
     data = environ["QUERY_STRING"]
-    data = "\n".join(data.split('&')) + "\n"
+    data = "\n".join(data.split('&'))
+    #data = 'Hello, world!'
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
-    return iter([data])
+    return iter([data.encode('utf-8')])
--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn hello:app
+sudo gunicorn3 -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 
